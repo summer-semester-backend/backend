1cd5a7c7ec5a1507dff44896cb9e616ecc388408
Wei Xia <1114891937@qq.com>
2022-07-29 18:41:30 +0800

    2

--- user/views.py
+++ user/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from django.views.decorators.csrf import csrf_exempt
 # Create your views here.
+
+@csrf_exempt
+def hello(request):
+    print("Hello world!")
