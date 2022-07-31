505f00fa3ac9f1cd20b1df9e1fbd4a40dc4ff070
Li Yu Heng <liyuheng55555@126.com>
2022-07-31 11:18:07 +0800

    add .gitignore, and mysql -> sqlite

--- backend/settings.py
+++ backend/settings.py
@@ -75,17 +75,17 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'ENFORCE_SCHEMA': True,
-        'NAME': 'summer',
-        'USER': 'rg',
-        'PASSWORD': '123',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'ENFORCE_SCHEMA': True,
+#         'NAME': 'summer',
+#         'USER': 'rg',
+#         'PASSWORD': '123',
+#         'HOST': '127.0.0.1',
+#         'PORT': '3306',
+#     }
+# }
 
 
 
