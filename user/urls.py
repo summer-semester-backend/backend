54bc935237c0f74ee31d4dd09337b220e8301903
Li Yu Heng <liyuheng55555@126.com>
2022-07-31 12:27:11 +0800

    try

--- user/urls.py
+++ user/urls.py
@@ -3,4 +3,5 @@ from .views import *
 
 urlpatterns = [
     path('hello', hello),
+    path('yoo', yoo),
 ]
--- user/views.py
+++ user/views.py
@@ -1,3 +1,4 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 # Create your views here.
@@ -5,3 +6,7 @@ from django.views.decorators.csrf import csrf_exempt
 @csrf_exempt
 def hello(request):
     print("小学期!")
+
+@csrf_exempt
+def yoo(request):
+    return JsonResponse({'msg':'yooo!'})
