a32815fd69af12b15dddb1cf1f09283f55efadea
Li Yu Heng <liyuheng55555@126.com>
2022-08-04 13:28:50 +0800

    增加File类型

--- project/models.py
+++ project/models.py
@@ -22,8 +22,52 @@ class Project(models.Model):
         on_delete=models.CASCADE,
     )
 
-    # def to_dic(self):
-    #     return {
-    #         'project_name': self.project_name,
-    #         'creator': self.creator.username,
-    #     }
+
+class FType:
+    root = 0
+    project = 1
+    directory = 2
+    uml = 12
+    prototype = 13
+    text = 14
+
+
+class File(models.Model):
+    fileID = models.AutoField(primary_key=True, editable=False)
+    file_image = models.CharField(max_length=50, null=True)
+    file_name = models.CharField(max_length=50)
+    create_time = models.DateTimeField(auto_now_add=True)
+    last_visit_time = models.DateTimeField(auto_now=True)
+    team = models.ForeignKey(
+        Team,
+        to_field='teamID',
+        on_delete=models.CASCADE,
+        null=True,
+    )
+    father = models.ForeignKey(
+        'File',
+        on_delete=models.CASCADE,
+        null=True,
+    )
+    file_creator = models.ForeignKey(
+        User,
+        to_field='userID',
+        on_delete=models.CASCADE,
+    )
+    data = models.TextField(default='')
+    is_deleted = models.BooleanField(default=False)
+    file_types = [
+        (FType.root, 'root'),
+        (FType.project, 'project'),
+        (FType.directory, 'directory'),
+        (FType.uml, 'uml'),
+        (FType.prototype, 'prototype'),
+        (FType.text, 'text'),
+    ]
+    type = models.IntegerField(choices=file_types)
+
+    def deletable(self):
+        return self.type >= FType.project
+
+    def is_dir(self):
+        return self.type <= FType.directory
