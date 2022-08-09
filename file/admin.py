5144995d28774fc37f6aa37cc7c81eea5bf8c762
Li Yu Heng <liyuheng55555@126.com>
2022-08-09 17:25:47 +0800

    admin

--- file/admin.py
+++ file/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import File   # 这个需要我们自己导入相应的模型类（数据表）
+from .models import File, Share   # 这个需要我们自己导入相应的模型类（数据表）
 
-admin.site.register([File])
+admin.site.register([File, Share])
 
 class FileAdmin(admin.ModelAdmin):
     list_display = ['fileID','file_name','file_image','team,','father','file_creator','data']
